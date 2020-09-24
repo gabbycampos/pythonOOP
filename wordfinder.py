239907f57c069eac4c27c17db33da0f1c3e2118c
Gabriela Campos <gabbycampos31@gmail.com>
2020-09-24 14:16:13 -0500

    inheritance

--- wordfinder.py
+++ wordfinder.py
@@ -3,21 +3,25 @@ import random
 
 class WordFinder:
     def __init__(self, filename):
-        # with open(filename) as f:
-        #     for line in f:
-        #         line = line.strip()
-        #         print(f"{len(self.words)} words read")
-
-        # filename = open("/usr/share/dict/words")
-        file = open(filename)
-        for word in file:
+        self.file = open(filename).read().split()
+        for word in self.file:
             word = word.strip()
             print(f"{len(word)} words read")
-      
-    
+
 
     def random(self):
-        return random.choice(word)
+        return random.choice(self.file)
 
-w = WordFinder("/Users/gabbycampos/Downloads/python-oo-practice/words.txt")
-print(w.random())
+# w = WordFinder("/Users/gabbycampos/Downloads/python-oo-practice/words.txt")
+# print(w.random())
+class SpecialWordFiner(WordFinder):
+    def __init__(self, filename):
+        super().__init__(filename)
+
+    def special(self):
+        for word in self.file:
+            if word is not word.startswith('#'):
+                return word
+
+newWord = SpecialWordFiner("/Users/gabbycampos/Downloads/python-oo-practice/wordsCopy.txt")
+print(newWord.random() in ["pear", "carrot", "kale"])
