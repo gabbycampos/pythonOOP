26e4d972783b3597611c46680b6573ee69d3f1cc
Gabriela Campos <gabbycampos31@gmail.com>
2020-10-01 17:47:38 -0500

    comment changes

--- wordfinder.py
+++ wordfinder.py
@@ -1,27 +1,31 @@
 """Word Finder: finds random words from a dictionary."""
 import random
 
+
 class WordFinder:
     def __init__(self, filename):
-        self.file = open(filename).read().split()
+        self.file = open(filename).read().split('\n')
+        # self.file = filter(None, self.file)
         for word in self.file:
             word = word.strip()
-            print(f"{len(word)} words read")
-
+            # print(f"{len(word)} words read")
 
     def random(self):
         return random.choice(self.file)
 
-# w = WordFinder("/Users/gabbycampos/Downloads/python-oo-practice/words.txt")
+# w = WordFinder("/Users/gabbycampos/Desktop/JavaScript_stuff/Springboard/Exercises/python-oo-practice/words.txt")
 # print(w.random())
-class SpecialWordFiner(WordFinder):
-    def __init__(self, filename):
-        super().__init__(filename)
 
+
+class SpecialWordFinder(WordFinder):
     def special(self):
         for word in self.file:
-            if word is not word.startswith('#'):
-                return word
+            # print(word)
+            word = word.strip()
+            if not word.startswith('#'):
+                print(word)
 
-newWord = SpecialWordFiner("/Users/gabbycampos/Downloads/python-oo-practice/wordsCopy.txt")
-print(newWord.random() in ["pear", "carrot", "kale"])
+
+newWord = SpecialWordFinder(
+    "/Users/gabbycampos/Desktop/JavaScript_stuff/Springboard/Exercises/python-oo-practice/wordsCopy.txt")
+print(newWord.special())
